3768c0f43d9d62fafb6ef8f077e2d8246cc0eeb4
Yashvinder <yashvinder.edx@gmail.com>
2017-07-05 22:49:41 +0530

    executor.sh for StandardVPC and HAStandardVPC added

---
+++ terraform/StandardVPC/executor.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+function logStatement() {
+  LOG_MESSAGE=$1
+  echo $LOG_MESSAGE
+  #echo $LOG_MESSAGE > /var/log/SimplePubVPCExecutor.log
+}
+
+function validateInputs() {
+  logStatement "Validating Inputs"
+}
+
+function createSetup() {
+  logStatement "Creating setup"
+
+  echo "aws_region = \"${aws_region}\"" > data.tfvars
+  echo "vpc_cidr = \"${vpc_cidr}\"" >> data.tfvars
+  echo "vpc_name = \"${vpc_name}\"" >> data.tfvars
+  echo "route53_zone_name = \"${zone_name}\"" >> data.tfvars
+  echo "public_subnet_aza_cidr = \"${public_subnet_aza_cidr}\"" >> data.tfvars
+  echo "private_subnet_aza_cidr = \"${private_subnet_aza_cidr}\"" >> data.tfvars
+
+  pushd ..
+  make setup-StandardVPC AWS_ACCESS_KEY_ID=$1 AWS_SECRET_ACCESS_KEY=$2
+  popd
+}
+
+function destroySetup() {
+  logStatement "Destroying Setup"
+
+  echo "aws_region = \"${aws_region}\"" > data.tfvars
+  echo "vpc_cidr = \"${vpc_cidr}\"" >> data.tfvars
+  echo "vpc_name = \"${vpc_name}\"" >> data.tfvars
+  echo "route53_zone_name = \"${zone_name}\"" >> data.tfvars
+  echo "public_subnet_aza_cidr = \"${public_subnet_aza_cidr}\"" >> data.tfvars
+  echo "private_subnet_aza_cidr = \"${private_subnet_aza_cidr}\"" >> data.tfvars
+
+  pushd ..
+  make destroy-StandardVPC AWS_ACCESS_KEY_ID=$1 AWS_SECRET_ACCESS_KEY=$2
+  popd
+}
