4ea49841b01bdd3ff041c9a9f95c7d0f9fb5d514
Sandeep Rawat <sandeep@opstree.com>
2017-07-06 00:03:39 +0530

    temporary check

--- terraform/ServerProvisioner/serverSetup.sh
+++ terraform/ServerProvisioner/serverSetup.sh
@@ -2,7 +2,8 @@
 rm -rf .terraform
 rm -f terraform.tfstate*
 terraform get
-terraform plan -var-file="data.tfvars"
-terraform apply -var-file="data.tfvars"
+#terraform plan -var-file="data.tfvars"
+#terraform apply -var-file="data.tfvars"
 server_ip=`terraform output server_ip`
+server_ip=34.205.45.215
 ssh -t -o StrictHostKeyChecking=no centos@${server_ip} "sudo cp ~/.ssh/authorized_keys /root/.ssh/authorized_keys"
