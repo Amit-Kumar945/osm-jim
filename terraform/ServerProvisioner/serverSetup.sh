2a59c3f07af7eed5d17355c7c496cb77538edb75
Sandeep Rawat <sandeep@opstree.com>
2017-07-06 00:13:03 +0530

    To test sudo tty issue

--- terraform/ServerProvisioner/serverSetup.sh
+++ terraform/ServerProvisioner/serverSetup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-rm -rf .terraform
-rm -f terraform.tfstate*
-terraform get
-terraform plan -var-file="data.tfvars"
-terraform apply -var-file="data.tfvars"
-server_ip=`terraform output server_ip`
+#rm -rf .terraform
+#rm -f terraform.tfstate*
+#terraform get
+#terraform plan -var-file="data.tfvars"
+#terraform apply -var-file="data.tfvars"
+#server_ip=`terraform output server_ip`
+server_ip=107.23.83.128
 ssh -t -o StrictHostKeyChecking=no centos@${server_ip} "sudo cp ~/.ssh/authorized_keys /root/.ssh/authorized_keys"
