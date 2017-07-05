83864308401df7b8712158b2877796262f452c53
Sandeep Rawat <sandeep@opstree.com>
2017-07-05 23:37:07 +0530

    Streamlining makefile

--- terraform/ServerProvisioner/serverSetup.sh
+++ terraform/ServerProvisioner/serverSetup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+rm -rf .terraform
+terraform get
+terraform plan -var-file="data.tfvars"
+terraform apply -var-file="data.tfvars"
 server_ip=`terraform output server_ip`
 ssh -t -o StrictHostKeyChecking=no centos@${server_ip} "sudo cp ~/.ssh/authorized_keys /root/.ssh/authorized_keys"
