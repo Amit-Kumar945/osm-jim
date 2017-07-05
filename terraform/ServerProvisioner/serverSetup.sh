339cb623178d06e46024c9b0fcc2a341066cce2f
Sandeep Rawat <sandeep@opstree.com>
2017-07-06 00:05:47 +0530

    serverSetup.sh

--- terraform/ServerProvisioner/serverSetup.sh
+++ terraform/ServerProvisioner/serverSetup.sh
@@ -5,5 +5,4 @@ terraform get
 terraform plan -var-file="data.tfvars"
 terraform apply -var-file="data.tfvars"
 server_ip=`terraform output server_ip`
-#server_ip=34.205.45.215
 ssh -t -o StrictHostKeyChecking=no centos@${server_ip} "sudo cp ~/.ssh/authorized_keys /root/.ssh/authorized_keys"
