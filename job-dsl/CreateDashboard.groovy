53009bdd392d25b1f8444c84188a429cd6455188
Your Name <you@example.com>
2017-07-06 15:00:15 +0530

    updated

--- job-dsl/CreateDashboard.groovy
+++ job-dsl/CreateDashboard.groovy
@@ -1,7 +1,38 @@
-buildMonitorView('techprimo-jobs')
-  description('All jobs for techprimo')
-    jobs {
-        names("BuildMonitor")
-        regex(/techprimo.+/)
-   }  
-  }
+job('ansible-command') {
+        parameters {
+     	extendedChoiceParameterDefinition {
+          name('Deployment_Group' )
+          type('Single Select' )
+          propertyFile('/var/lib/jenkins/inventory.Properties')
+          propertyKey('key')
+          defaultValue('targets')
+          defaultPropertyFile('')
+          value('')
+          defaultPropertyKey('') 
+          quoteValue(false)
+          visibleItemCount(5) 
+       	  description('')
+          multiSelectDelimiter('')
+          bindFieldName('')
+          svnPath(false)
+          svnUrl('')
+          svnUserName('')
+          svnPassword('')
+          projectName('')
+          roleBasedFilter(false)
+        }
+          stringParam('Command')
+}
+  
+   label('{{LABEL}}')
+
+steps {
+    shell(
+
+    'name=$Command\n'  +
+    
+'ansible ${Deployment_Group} -a  "$name"\n' 
+
+)
+}
+}
